6490ee4f73f56b15264b88868a61d548f3d8b54a
Sash <sash@binary.com>
2018-03-06 13:12:50 +0200

    update

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,12 +1,1 @@
-// test popup
-var body = document.body;
-window.onload = function() {
-   modal.style.display = "block";
-   body.classList.add("lock");
-};
-var modal = document.getElementById('jpModal');
-var acc = document.getElementById("confirm");
-acc.onclick = function() {
-    modal.style.display = "none";
-    body.classList.remove("lock");
-}
+var body=document.body;window.onload=function(){modal.style.display="block",body.classList.add("lock")};var modal=document.getElementById("jpModal"),acc=document.getElementById("confirm");acc.onclick=function(){modal.style.display="none",body.classList.remove("lock")};
